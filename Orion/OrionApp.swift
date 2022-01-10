1718b319badd56ef45625740e737442fc97c82a1
Kaiser Pister <pisterk@gmail.com>
2022-01-10 16:35:16 -0600

    Initial Commit

---
+++ Orion/OrionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrionApp.swift
+//  Orion
+//
+//  Created by Kaiser Pister on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct OrionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
